422959b6325855e5c4479ad4b038829d079c3c3a
asankheya <lilitasa@proton.me>
2025-05-05 21:09:04 +0700

    Update users urls

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    # Здесь позже добавим пути
+    path('', views.UserListView.as_view(), name='index'),
+    path('create/', views.UserCreateView.as_view(), name='create'),
+    path('<int:pk>/update/', views.UserUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='delete'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
