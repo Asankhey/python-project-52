3b13e1fa7d83ae718ae7143cfcabaadc0062f41a
asankheya <lilitasa@proton.me>
2025-05-17 22:47:43 +0700

    Add task filtering with django-filter

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,18 +1,19 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
-from django.views.generic import (
-    ListView, CreateView, DetailView, UpdateView, DeleteView
-)
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django_filters.views import FilterView
 
 from task_manager.models import Task
 from task_manager.forms import TaskForm
+from task_manager.tasks.filters import TaskFilter
 
 
-class TaskListView(LoginRequiredMixin, ListView):
+class TaskListView(LoginRequiredMixin, FilterView):
     model = Task
     template_name = 'tasks/task_list.html'
     context_object_name = 'tasks'
+    filterset_class = TaskFilter
 
 
 class TaskCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
@@ -48,4 +49,4 @@ class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixi
     success_message = 'Задача успешно удалена'
 
     def test_func(self):
-        return self.get_object().author == self.request.user
+        return self.request.user.pk == self.get_object().author.pk
