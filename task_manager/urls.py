c1957599b669528718a7f2c3a7db892ff0e6d740
asankheya <lilitasa@proton.me>
2025-06-02 19:41:53 +0700

    Fix templates and update views

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from task_manager.tasks.views import trigger_error  # удалили manual_rollbar_test
+from task_manager.views.index import IndexView
+from task_manager.tasks.views import trigger_error  # только trigger_error
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='home'),  # 👈 маршрут для главной страницы
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('statuses/', include('statuses.urls', namespace='statuses')),
