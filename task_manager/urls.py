e012596a9448cf9675d21f132f23f9686acb3ff8
asankheya <lilitasa@proton.me>
2025-06-09 19:21:22 +0700

    update urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,8 +4,8 @@ from task_manager.tasks.views import trigger_error  # удалили manual_roll
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
-    path('statuses/', include('statuses.urls', namespace='statuses')),
+    path('users/', include('task_manager.users.urls', namespace='users')),
+    path('statuses/', include('task_manager.statuses.urls', namespace='statuses')),
     path('tasks/', include('task_manager.tasks.urls', namespace='tasks')),
     path('labels/', include('task_manager.labels.urls', namespace='labels')),
     path('rollbar-debug/', trigger_error),  # путь для генерации ошибки
