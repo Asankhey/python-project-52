10d1791ac5f4bf687bf4693ded825b9fb3eee979
asankheya <lilitasa@proton.me>
2025-06-09 19:29:45 +0700

    settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -12,7 +12,15 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-placeholder')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'webserver').split(',')
+import os
+
+raw_hosts = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1')
+hosts = [h.strip() for h in raw_hosts.split(',') if h.strip()]
+
+if 'webserver' not in hosts:
+    hosts.append('webserver')
+
+ALLOWED_HOSTS = hosts
 
 INSTALLED_APPS = [
     'django.contrib.admin',
