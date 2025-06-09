39037076796ea85c04af7a758a2950c49d168b89
asankheya <lilitasa@proton.me>
2025-06-09 19:12:21 +0700

    update all

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-from statuses.models import Status
+from task_manager.statuses.models import Status
 
 User = get_user_model()
 
--- task_manager/tests.py
+++ task_manager/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
-from statuses.models import Status
+from task_manager.statuses.models import Status
 from task_manager.models import Task
 
 User = get_user_model()
