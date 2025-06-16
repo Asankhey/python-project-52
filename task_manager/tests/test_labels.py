1b94db03cbb63de4eaa9b7adf6fcfb03b8afc705
asankheya <lilitasa@proton.me>
2025-06-17 00:04:36 +0700

    update test_labels

--- task_manager/tests/test_labels.py
+++ task_manager/tests/test_labels.py
@@ -1,85 +1,66 @@
 from django.test import TestCase
 from django.urls import reverse
-
 from task_manager.labels.models import Label
 from task_manager.users.models import User
 
 
 class LabelTestCase(TestCase):
-    fixtures = ["user_test", "label_test"]
-
-    def test_access(self):
-        resp1 = self.client.get(reverse('create_label'))
-        self.assertEqual(resp1.status_code, 302)
-        resp1 = self.client.get(reverse('labels'))
-        self.assertEqual(resp1.status_code, 302)
-        resp1 = self.client.get(reverse('update_label', kwargs={'pk': 1}))
-        self.assertEqual(resp1.status_code, 302)
-        resp1 = self.client.get(reverse('delete_label', kwargs={'pk': 1}))
-        self.assertEqual(resp1.status_code, 302)
-
-        self.login()
-        resp1 = self.client.get(reverse('create_label'))
-        self.assertEqual(resp1.status_code, 200)
-        resp1 = self.client.get(reverse('labels'))
-        self.assertEqual(resp1.status_code, 200)
-        resp1 = self.client.get(reverse('update_label', kwargs={'pk': 1}))
-        self.assertEqual(resp1.status_code, 200)
-        resp1 = self.client.get(reverse('delete_label', kwargs={'pk': 1}))
-        self.assertEqual(resp1.status_code, 200)
+    fixtures = [
+        'user_test.json',
+        'label_test.json',
+    ]
 
     def login(self):
-        user = User.objects.get(id=1)
-        self.client.force_login(user)
+        self.client.force_login(User.objects.get(pk=1))
 
     def test_ListLabels(self):
         self.login()
         resp = self.client.get(reverse('labels'))
-        self.assertTrue(len(resp.context['labels']) == 2)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(
+            resp,
+            template_name='labels/index.html'
+        )
+        self.assertEqual(len(resp.context['labels']), 4)
+        names = [label.name for label in resp.context['labels']]
+        self.assertIn('bug', names)
+        self.assertIn('urgent', names)
 
     def test_CreateLabel(self):
         self.login()
         resp = self.client.get(reverse('create_label'))
         self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, template_name='general/general_form.html')
+        self.assertTemplateUsed(
+            resp,
+            template_name='general/general_form.html'
+        )
 
         resp = self.client.post(reverse('create_label'), {
-            'name': 'test',
+            'name': 'test-label',
         })
         self.assertEqual(resp.status_code, 302)
-        self.assertRedirects(resp, reverse('labels'))
+        self.assertRedirects(
+            resp,
+            reverse('labels')
+        )
+
         label = Label.objects.last()
-        self.assertEqual(label.name, 'test')
+        self.assertEqual(label.name, 'test-label')
 
         resp = self.client.get(reverse('labels'))
-        self.assertTrue(len(resp.context['labels']) == 3)
-
-    def test_UpdateLabel(self):
-        self.login()
-        label = Label.objects.get(id=1)
-        resp = self.client.get(
-            reverse('update_label', kwargs={'pk': label.id})
-        )
-        self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, template_name='general/general_form.html')
-        resp = self.client.post(
-            reverse('update_label', kwargs={'pk': label.id}), {
-                'name': 'test_label'
-            })
-        self.assertEqual(resp.status_code, 302)
-        label.refresh_from_db()
-        self.assertEqual(label.name, 'test_label')
+        self.assertEqual(len(resp.context['labels']), 5)
+        names = [label.name for label in resp.context['labels']]
+        self.assertIn('test-label', names)
 
     def test_DeleteLabel(self):
         self.login()
-        label = Label.objects.get(name="home-test")
-        resp = self.client.get(
-            reverse('delete_label', kwargs={'pk': label.id})
-        )
-        self.assertEqual(resp.status_code, 200)
+        label = Label.objects.get(name="bug")
         resp = self.client.post(
-            reverse('delete_label', kwargs={'pk': label.id})
+            reverse('delete_label', args=[label.id])
         )
-        self.assertRedirects(resp, reverse('labels'))
         self.assertEqual(resp.status_code, 302)
-        self.assertEqual(Label.objects.count(), 1)
+        self.assertRedirects(
+            resp,
+            reverse('labels')
+        )
+        self.assertFalse(Label.objects.filter(id=label.id).exists())
