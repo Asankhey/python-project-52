2288b96f1cdf6bf7d6bbb0faa5f47b8595909ac8
asankheya <lilitasa@proton.me>
2025-06-17 02:13:48 +0700

    fix sonar

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key-for-tests')
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key-for-tests')  # NOSONAR
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 RENDER_EXTERNAL_HOSTNAME = os.getenv('RENDER_EXTERNAL_HOSTNAME')
